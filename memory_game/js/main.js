6b166da052eafdc30235fc84844886974a5b8422
Yousef Hussin <yousef_hussin@yahoo.com>
2020-03-09 20:00:12 -0700

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,7 @@
+let cardOne = "King";
+let cardTwo = "King";
+let cardThree = 'Queen';
+let cardFour = 'Queen';
+
+console.log('User flipped: ' + cardOne);
+console.log('User flipped: ' + cardThree);
