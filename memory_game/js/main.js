fde437be128003d08d8150fc55b3fb415fb5102f
Yousef Hussin <yousef_hussin@yahoo.com>
2020-03-10 17:56:48 -0700

    Created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,17 @@
-let cardOne = "King";
-let cardTwo = "King";
-let cardThree = 'Queen';
-let cardFour = 'Queen';
+let cards = ['queen', 'queen', 'king', 'king']
+let cardsInPlay = []
+let cardOne = cards[0];
+cardsInPlay.push(cardOne);
+let cardTwo = cards[2];
+cardsInPlay.push(cardTwo)
 
-console.log('User flipped: ' + cardOne);
-console.log('User flipped: ' + cardThree);
+if (cardsInPlay.length === 2) {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    alert("You found a match!")
+  } else {
+    alert("Try again")
+  }
+}
+
+console.log('User flipped ' + cardOne);
+console.log('User flipped ' + cardTwo);
